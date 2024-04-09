d65fec029d8ccd31d2068b6b1a894fa291d8e7be
Henry Owens <henryowens@Henrys-MacBook-Pro.local>
2024-04-09 20:50:28 +0200

    Removing generated files

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./models/prisma";
-export * from "./models/project";
--- dist/models/prisma.d.ts
+++
@@ -1,9 +0,0 @@
-export type PrismaInput<R> = Omit<R, "id">;
-type BaseRequest<D, P = {}> = {
-    data: D;
-    params: P;
-};
-export type RequestWithProjectId<D> = BaseRequest<D, {
-    projectId: string;
-}>;
-export {};
